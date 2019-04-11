f9999f6e41727771fc5d412cdb88d55bea5530d7
sr.weloper <sen.webdev@hotmail.com>
2019-04-11 05:56:06 -0700

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from flask_jwt_extended import JWTManager
 from flask_jwt_extended import (create_access_token, create_refresh_token, jwt_required, jwt_refresh_token_required, get_jwt_identity, get_raw_jwt)
 import os
 
-DEBUT=True
+DEBUT=True    
 
 app=Flask(__name__)
 app.config.from_object(__name__)
